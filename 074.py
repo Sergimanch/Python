2d5dbbdb029950f815c95407a2592bfb315872fc
Sergimanch <lopezlianessergio@gmail.com>
2025-11-19 11:43:32 +0100

    nuevos ejs p1

---
+++ 074.py
@@ -0,0 +1,3 @@
+engesp={"hola":"hello", "bueno":"good", "adios":"bye", "español":"spanish", "gato":"cat", "ella":"her"}
+palabra= input("Introduce una palabra del diccionario: ").lower()
+print(f"La palabra {palabra} es {engesp[palabra]}")
---
+++ 075.py
@@ -0,0 +1,3 @@
+listacompra={"manzana":1, "pera":4, "sandia":2, "melon":3, "naranja":2, "caqui":5}
+for i in listacompra:
+    print(listacompra[i] , " " ,i)
